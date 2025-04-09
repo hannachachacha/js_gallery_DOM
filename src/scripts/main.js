bf64c9f9ef6fab76ae793038335cf8e532af449a
Hanna <hannachachacha7@gmail.com>
2025-04-09 13:05:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__large-img');
+const miniImgs = document.querySelector('ul#thumbs');
+
+miniImgs.onclick = () => {
+  event.preventDefault();
+
+  let link;
+
+  if (event.target.tagName === 'IMG') {
+    link = event.target.parentElement;
+  }
+
+  mainImage.src = link.href;
+};
