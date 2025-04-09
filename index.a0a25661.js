6f1ff365156c56347993b3835efd826f11cc1cc4
Hanna <hannachachacha7@gmail.com>
2025-04-09 13:06:21 +0300

    make build

---
+++ index.a0a25661.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector("ul#thumbs").onclick=function(){var t;event.preventDefault(),"IMG"===event.target.tagName&&(t=event.target.parentElement),e.src=t.href};
+//# sourceMappingURL=index.a0a25661.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const mainImage = document.querySelector(".gallery__large-img");
+const miniImgs = document.querySelector("ul#thumbs");
+miniImgs.onclick = ()=>{
+    event.preventDefault();
+    let link;
+    if (event.target.tagName === "IMG") link = event.target.parentElement;
+    mainImage.src = link.href;
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
